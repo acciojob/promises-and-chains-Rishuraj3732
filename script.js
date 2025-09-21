0de320fc3c234c024014b3eda97b8fa69956572d
Rishuraj3732 <103851611+Rishuraj3732@users.noreply.github.com>
2025-09-21 17:15:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-//your JS code here. If required.
-const form=document.getElementById("voteForm");
-form.addEventListener("submit",function event() {
-	event.preventDefault();
-	const name=document.getElementById("name").value.trim();
-	const age=document.getElementById("age").value.trim();
-	if(!name || !age){
-		alert("Please enetr valid details");
-		return;
-	}
-	const checkAge=new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			if (parseInt(age) > 18) {
-            resolve(`Welcome, ${name}. You can vote.`);
-          } else {
-            reject(`Oh sorry ${name}. You aren't old enough.`);
-          }
-			
-		},4000);
-	});
-	checkAge
-        .then(message => alert(message))
-        .catch(error => alert(error));
-	
+const form = document.getElementById("voteForm");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // stop form reload
+
+  const name = document.getElementById("name").value.trim();
+  const age = document.getElementById("age").value.trim();
+
+  if (!name || !age) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const checkAge = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(age) > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  checkAge
+    .then(msg => alert(msg))
+    .catch(err => alert(err));
 });
